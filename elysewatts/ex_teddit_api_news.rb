7e8410d75bd98916531bfeab1d76a5d756c1441d
Elyse Watts <wattsupelyse@gmail.com>
2015-03-05 21:29:53 -0800

    adding all the stuff i've been working on

---
+++ elysewatts/ex_teddit_api_news.rb
@@ -0,0 +1,26 @@
+# We're going to add a remote data source to pull in stories from Mashable and Digg.
+  # http://mashable.com/stories.json
+  # http://digg.com/api/news/popular.json
+# These stories will also be upvoted based on our rules. No more user input!
+
+# Pull the json, parse it and then make a new story hash out of each story(Title, Category, Upvotes)
+# Add each story to an array and display your "Front page"
+require 'json'
+require 'rest-client'
+
+
+json = RestClient.get('http://reddit.com/.json')
+result = JSON.load json
+
+story = {}
+
+
+result['data']['children'].each do |story|
+	title = story['data']['title']
+	category = story['data']['subreddit']
+	upvotes = story['data']['ups']
+	stories['title'] = title
+end
+
+
+
