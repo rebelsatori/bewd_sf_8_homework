423500f3de7ed6fe0b3be3b2422d05b4fda3a745
Raymond Torresr01 <Torresr01@me.com>
2015-03-05 17:28:08 -0800

    Completed Functionality/Ending Issues

--- secretnumber.rb
+++ secretnumber.rb
@@ -3,26 +3,43 @@ puts 'Hello! Welcome to Secret Number! My name is Raymond and I am the creator a
 puts ' '
 pname = gets.chomp
 puts ' '
-puts 'Hello ' + pname + '! The rules are fairly simple.' 
+puts 'Hello ' + pname + '! The rules are pretty simple.' 
 puts 'You have 3 attempts to guess a random number betwenn 1 and 10.'
 puts 'To start, just make your first guess and press enter. Good Luck' #intro/rules
-secnum = (rand(10)+1).to_i
+secnum = rand(1..10).to_i
 puts secnum
 guess = gets.chomp.to_i	 	#Functionality: firstguess
-count = 1.to_i
+attempts = 1.to_i
+tries = 3.to_i
 
-while count!=3 #loops while wrong guess
+while tries!=0 #loops while wrong guess
 	
-	if secnum != guess
-	count = count+1	
-	puts 'Ugh that was not the right guess. Try again'
-	guess = gets.chomp.to_i
-	elsif guess == secnum 
-	puts 'Good job! You chose the right number!!'
-	count= count+3
+	while secnum !=guess
+		if secnum<guess
+			attempts = attempts+1
+			tries = tries-1	
+			puts 'Too High!'
+			puts 'You have ' + tries.to_s + ' attempts left. Give it another try!'
+			guess = gets.chomp.to_i
+		
+		elsif secnum> guess
+			attempts = attempts+1
+			tries = tries-1
+			puts 'Too Low!'
+			puts 'You have ' + tries.to_s + ' attempts left. Give it another try!'
+			guess = gets.chomp.to_i	
+		end 
 	end
-	
-	if count==3
-		puts 'i\'m sorry, you chose wrong.'
-	end	
+
+	if guess == secnum 
+	puts guess.to_s + ' Was the right number! Good job!!' 
+	tries == 0
+	end
+
+	if tries == 0
+		puts 'Nice Try ' + pname + ', but better luck next time!'
+		puts 'The numebr you were looking for was ' + secnum.to_s
+		tries == 0
+	end
 end
+
