b5ed1f90987227b4fdf2a6ad6d5b519f902a60c8
Matt Brown <mbrown87@gmail.com>
2015-03-10 16:32:28 -0700

    Finished adding comments

--- MattBrown-matttbrown/github_api_search.rb
+++ MattBrown-matttbrown/github_api_search.rb
@@ -1,4 +1,4 @@
-# Make sure we're using gems to get and convert data
+# Add gems needed to get and convert data
 require 'json'
 require 'rest-client'
 
@@ -6,11 +6,13 @@ require 'rest-client'
 puts 'Enter a search term for the Ruby repo\'s on Github:'
 query = gets.chomp
 
-# Run the search using the term supplied above, and convert JSON results to a hash
+# Run the search using the given term, and convert JSON results to a hash
 result_hash = JSON.load(RestClient.get("https://api.github.com/search/repositories?q=" + query + "+language:ruby"))
 
+# Tell the user how many results there are
 puts "There are " + result_hash['total_count'].to_s + " results. \n\n"
 
+# Iterate though the hash and return the top repo titles, owners, and number of stars.  Note that Github throttles results to 30
 result_hash['items'].each do |idx|
 	puts "The repo " + idx['name'] + " was made by " + idx['owner']['login'] + " and has "  + idx['stargazers_count'].to_s + " stars."
 end
