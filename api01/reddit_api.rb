80cece226256d3f45608e22f5d3e4224a9b61320
Raymond Torresr01 <Torresr01@me.com>
2015-03-10 18:40:43 -0700

    API 01

---
+++ api01/reddit_api.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'rest-client'
+json_reddit = RestClient.get('https://www.reddit.com/.json')
+reddit = JSON.load json_reddit
+
+def build_story(title, ups, url)
+	"Title: " + title + 
+	"\nUps: " + ups +
+	"\nURL: " + url + "\n "
+end
+
+reddit['data']['children'].each do |story|
+	title = story['data']['title']
+	ups = story['data']['ups'].to_s
+	url = story['data']['url']
+	puts build_story(title, ups, url)
+end
