a43e06229abb1d4253ec241c086792db233a69ee
Elyse Watts <wattsupelyse@gmail.com>
2015-03-16 00:32:40 -0700

    created framework for midterm assignment

---
+++ elysewatts/midterm/main.rb
@@ -0,0 +1,10 @@
+
+require 'some gem that will be useful'
+
+require_relative 'lib/some file that i will need'
+
+
+### Requirements ###
+# Takes user input and handles bad input (i.e. You expect numbers and they give you a letter).
+# Connects to a 3rd party api where the call varies based on what the user gives you.
+# Must be object oriented. You must instantiate classes. The only file that can run without classes is your main.rb
