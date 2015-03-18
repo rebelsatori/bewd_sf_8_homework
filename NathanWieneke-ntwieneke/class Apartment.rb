fbcc30e9bd1216049969de286ed3126ab0bb572d
ntwieneke <nathanwieneke@gmail.com>
2015-03-17 18:37:06 -0700

    stuck on secretnumberobject hw

---
+++ NathanWieneke-ntwieneke/class Apartment.rb
@@ -0,0 +1,14 @@
+class Apartment
+	attr_accessor :name, :rent, :sqft, :num_bedrooms, :number_bathrooms
+end
+
+class Apartment
+	def initialize(name, rent, sqft, num_bedrooms, number_bathrooms)
+		@name = name
+		@rent = rent
+		@sqft = sqft
+		@num_bedrooms = num_bedrooms
+		@number_bathrooms = number_bathrooms
+	end
+end
+classy_apartment = Apartment.new('Classy Apartment', 15000, 15000, 5, 5)
