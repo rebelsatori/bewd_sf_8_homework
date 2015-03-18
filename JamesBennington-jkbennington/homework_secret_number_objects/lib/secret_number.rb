e642b8ccbbe1f46c4220b32837a0561d4c8be02b
jkbennington <jkbennington@gmail.com>
2015-03-17 18:27:54 -0700

    Added my broken SecretNumber objects code

---
+++ JamesBennington-jkbennington/homework_secret_number_objects/lib/secret_number.rb
@@ -0,0 +1,12 @@
+# this class will generate our secret number for us
+class SecretNumber
+   attr_accessor :secret_number
+   	
+   	def initialize (secret_number)
+   	@secret_number = secret_snumber
+   	end
+   	secret_number = rand(10)
+  
+
+end
+    
