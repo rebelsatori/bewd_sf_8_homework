d4145cc4835a12914308bcc36555f0669deb637e
jkbennington <jkbennington@gmail.com>
2015-03-23 15:44:49 -0700

    Updating Games

--- JamesBennington-jkbennington/games/config/routes.rb
+++ JamesBennington-jkbennington/games/config/routes.rb
@@ -3,4 +3,6 @@ Games::Application.routes.draw do
   
   resources 'games', only: [:index]
   resources "secret_numbers", only: [:new, :show]
+  resources "rock_paper_scissors", only: [:new, :show]
+  resources "scramble", only: [:new, :show]
 end
