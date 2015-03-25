e1949e6e77d1d47ab4129c825d26d05ac653c75a
marygliu <marygliu@gmail.com>
2015-03-24 20:38:41 -0700

    Games on Rails -- Rock Paper Scissors & Secret Number

--- MaryLiu-marygliu/games/app/controllers/rock_paper_scissors_controller.rb
+++ MaryLiu-marygliu/games/app/controllers/rock_paper_scissors_controller.rb
@@ -1,19 +1,28 @@
+#@Bryan/@David: Please help here. When game_throw = scissors and user_throw = rock, it's not working. 
+
 class RockPaperScissorsController < ApplicationController
   def new
+    @rps = ['rock', 'paper', 'scissor']
   end
 
   def show
-    @throw_number = params[:id].to_i
-    @throw = rand(0..2)
-    #@rock = 0
-    #@paper = 1
-    #@scissors = 2
-    @player_won = (@throw_number == @throw)
+    @rps = ['rock', 'paper', 'scissor']
+    @user_throw = params[:id]
+    @game_throw = @rps[rand(0..2)]
 
-    if @player_won
+    win = 
+       {'rock' => 'scissors', 
+        'paper' => 'rock',
+        'scissors' => 'paper'}
+
+    if win[@user_throw] == @game_throw
       @outcome_partial = 'win'
+    elsif @user_throw == @game_throw
+      @outcome_partial = 'tie'
     else
       @outcome_partial = 'lose'
     end
   end
+
+
 end
