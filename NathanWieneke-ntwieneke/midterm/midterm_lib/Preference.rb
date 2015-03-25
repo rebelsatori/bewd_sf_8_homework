0dd7c55d0bc9268d85a73d841e63ace2d4721ea7
ntwieneke <nathanwieneke@gmail.com>
2015-03-24 18:21:25 -0700

    finished

--- NathanWieneke-ntwieneke/midterm/midterm_lib/Preference.rb
+++ NathanWieneke-ntwieneke/midterm/midterm_lib/Preference.rb
@@ -1,11 +1,10 @@
 class Preference
 	attr_accessor :yes, :like, :check
-	def initialize(yes)
+	def initialize
 		@yes = 0
 	end
 	def like
 		x = 0
-		y = 0
 		while x < 4 do
 			new_reddit = Reddit.new
 			new_reddit.fetch_stories
@@ -13,16 +12,16 @@ class Preference
 			puts "Do you like this headline?"
 			user_like = gets.chomp
 				if user_like == 'yes'
-					y = y + 1
+					@yes = @yes + 1
 					x = x +1
-					puts "y is at #{y}"
-					puts "x is at #{x}"
-					@yes << y
-					puts @yes
+					# puts "y is at #{y}"
+					#puts "x is at #{x}"
+					#puts "yes is at #{@yes}"
 				elsif user_like == 'no'
 					x = x + 1
-					puts "y is at #{y}"
-					puts "x is at #{x}"
+					# puts "y is at #{y}"
+					#puts "x is at #{x}"
+					#puts "yes is at #{@yes}"
 				else
 					puts "please enter (yes) or (no)"
 				end
@@ -34,10 +33,10 @@ class Preference
 		if @yes > 2
 			puts "You are easy to please!"
 		else 
-			puts "You are picky, here is another story"
-			new_reddit = Reddit.new
-			new_reddit.fetch_stories
-			puts new_reddit.stories[5]
+			puts "You are picky!"
+			#new_reddit = Reddit.new
+			#new_reddit.fetch_stories
+			#puts new_reddit.stories[5]
 		end
 	end
 end
